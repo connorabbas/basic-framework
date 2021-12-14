7c91371f0048ee2b9e2d62dd7b2a0474d092579e
connorabbas <connora@jdsindustries.com>
2021-12-13 19:47:27 -0600

    fix

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 
 // Autoload Classes
 spl_autoload_register(
-    function($class)
-    {
+    function ($class) {
         if (file_exists('./app/core/' . $class . '.php')) {
             require_once './app/core/' . $class . '.php';
         } else if (file_exists('./app/controllers/' . $class . '.php')) {
@@ -15,11 +14,11 @@ spl_autoload_register(
     }
 );
 
-// Include DB connection once, use dependency injection for with constructors for DB usage
+// Include DB connection once, use dependency injection with class constructors for DB usage
 require_once '../DATABASE/pdo_db.class.php';
 global $db;
 $db = new db();
 
 // Global vars and routes
 require_once('./app/globals.php');
-require_once('./app/Routes.php');
+require_once('./app/routes.php');
