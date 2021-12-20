c301d2cbfa119cea78af09dbc01043d895e309fc
Connor Abbas <87775611+cabbas23@users.noreply.github.com>
2021-12-20 10:44:44 -0600

    Update App.php

--- app/core/App.php
+++ app/core/App.php
@@ -9,6 +9,11 @@ class App
             }
         }
         // Include view file
-        require_once('./app/views/' . $view . '.php');
+        if(file_exists('./app/views/' . $view . '.php')){
+            require_once('./app/views/' . $view . '.php');
+        } else{
+            require_once('./app/views/404.php');
+            die($_404);
+        }
     }
 }
