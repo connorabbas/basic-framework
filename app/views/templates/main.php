986361f9fe49739c0c1d1d67e8a391f22a81dcd3
connorabbas <connora@jdsindustries.com>
2022-01-09 14:08:43 -0600

    small update

--- app/views/templates/main.php
+++ app/views/templates/main.php
@@ -19,7 +19,7 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">php-mf</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
