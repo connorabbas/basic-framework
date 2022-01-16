1401a99cfe9bfa47b45864e53b4af327ac02c8b7
connorabbas <connora@jdsindustries.com>
2022-01-16 10:50:45 -0600

    updates

--- app/views/templates/main.template.php
+++ app/views/templates/main.template.php
@@ -16,7 +16,7 @@
         <meta property="og:url" content="<?= URL ?>" />
         <meta property="og:site_name" content="JDS Industries" />
         <meta property="og:title" content="JDS Industries - <?= $pageTitle ?>" />
-        <meta property="og:description" content="<?=$pageDesc?>" />
+        <meta property="og:description" content="<?= $pageDesc ?>" />
         <meta property="og:image" content="<?= BASE_DIR ?>resources/images/php-logo.png" />
         <meta property="og:image:width" content="1280" />
         <meta property="og:image:height" content="670" />
