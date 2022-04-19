41662c3b7b906f3d09ff0f1210f6e057bbcf8d85
connorabbas <connora@jdsindustries.com>
2022-04-18 21:54:09 -0500

    dynamic routing

--- app/routes/public.php
+++ app/routes/public.php
@@ -13,4 +13,8 @@ $router->get('/', function() {
     return App::view('pages/welcome');
 });
 
+$router->get('/test/$var', function() {
+    echo 'test with ' . $_REQUEST['var'];
+});
+
 $router->get('/tester', [TestController::class, 'index']);
