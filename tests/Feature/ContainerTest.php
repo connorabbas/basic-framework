b1492bd22575fcc0cd2d267cd891dde15e52f6e6
connorabbas <abbasconnor@gmail.com>
2023-01-11 09:03:10 -0600

    extra tests

---
+++ tests/Feature/ContainerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    public function testCase()
+    {
+        $this->assertTrue(true);
+    }
+}
