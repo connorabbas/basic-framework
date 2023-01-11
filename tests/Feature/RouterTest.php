bf13b22fc3f3753509b2a04cd9bb8f7668e5d3e3
connorabbas <abbasconnor@gmail.com>
2023-01-10 20:18:47 -0600

    setup testing

---
+++ tests/Feature/RouterTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test actual endpoints within the site
+ * Site must be served
+ */
+class RouterTest extends TestCase
+{
+    private $client;
+
+    public function setUp(): void
+    {
+        $this->client = new Client(['base_uri' => 'http://127.0.0.1:8000']);
+    }
+
+    public function testRegisteredRoute()
+    {
+        $response = $this->client->get('/example');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testNonRegisteredRoute()
+    {
+        $response = $this->client->get('/abc/123', ['http_errors' => false]);
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+}
---
+++ tests/Unit/RouterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Core\Router;
+use App\Core\Container;
+use PHPUnit\Framework\TestCase;
+use App\Controllers\ExampleController;
+
+class RouterTest extends TestCase
+{
+    private $container;
+
+    public function setUp(): void
+    {
+        $this->container = new Container();
+    }
+
+    public function testRegisterRoute()
+    {
+        $router = new Router($this->container);
+        $expected = [
+            'GET' => [
+                '/test' => [ExampleController::class, 'test']
+            ]
+        ];
+
+        $router->register('GET', '/test', [ExampleController::class, 'test']);
+
+        $this->assertEquals($router->getRoutes(), $expected);
+    }
+
+    public function testRegisterGetRoute()
+    {
+        $router = new Router($this->container);
+        $expected = [
+            'GET' => [
+                '/test' => [ExampleController::class, 'test']
+            ]
+        ];
+
+        $router->register('GET', '/test', [ExampleController::class, 'test']);
+
+        $this->assertEquals($router->getRoutes(), $expected);
+    }
+}
